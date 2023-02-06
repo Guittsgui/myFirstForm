b7b345e84415d8cbab120fc0e88e5b95a917d65d
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-02-05 22:10:33 -0300

    css finalizado

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const listaRegistros = []
+const container = document.querySelector('.container')
+const loginBox = document.querySelector('.loginbox')
+const registerBox = document.querySelector('.registerbox')
+const pgLogin = document.querySelector('.pgLogin').addEventListener('click',()=>{
+   registerBox.style.display='none'
+})
+const pgRegister = document.querySelector('.pgRegister').addEventListener('click',()=>{
+    
+})
