201f1058640ad100574c3887cb79c284b33ae546
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-02-05 22:15:56 -0300

    flip pagina funcionando

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const container = document.querySelector('.container')
 const loginBox = document.querySelector('.loginbox')
 const registerBox = document.querySelector('.registerbox')
 const pgLogin = document.querySelector('.pgLogin').addEventListener('click',()=>{
-   registerBox.style.display='none'
+   loginBox.style.display='none'
+   registerBox.style.display='flex'
 })
 const pgRegister = document.querySelector('.pgRegister').addEventListener('click',()=>{
-    
+    registerBox.style.display='none'
+    loginBox.style.display='flex'
 })
